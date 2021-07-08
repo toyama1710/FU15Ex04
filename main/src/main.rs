6d29d47008eee26fab15bf5c4a19cebb4b9d1e7f
toyama1710 <toyama@toyama1710.net>
2021-07-08 23:36:45 +0900

    Won/lost message added

--- main/src/main.rs
+++ main/src/main.rs
@@ -4,4 +4,13 @@ fn main() {
     let (a1, a2) = (rand::random::<u8>() % 6 + 1, rand::random::<u8>() % 6 + 1);
     println!("Die 1: {}", a1);
     println!("Die 2: {}", a2);
+
+    println!("Total value: {}", a1 + a2);
+
+    if a1 + a2 > 7 {
+        println!("You won!");
+    }
+    else {
+        println!("You lost");
+    }
 }
